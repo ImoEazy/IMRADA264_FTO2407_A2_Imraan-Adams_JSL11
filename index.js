541f38277dfd5fca7c93542f4582827709436484
ImoEazy <imieadams@gmail.com>
2024-10-03 22:07:10 +0200

    added import getTasks/initialData

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TASK: import helper functions from utils
-
+import { getTasks, createNewTask } from "./utils/taskFunctions";
 // TASK: import initialData
-
+import { initialData } from "./initialData";
 
 
 /*************************************************************************************************************************************************
